c4d16cb52d4332c3b320dde498f4478148ee4d8f
nawafali01 <nawafali3435@gmail.com>
2026-02-28 16:40:20 +0500

    fix route issue

--- router/route.jsx
+++ router/route.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Layout from '../layout/Layout'
 import NotFound404 from '../src/components/NotFound404'
 import Home from '../src/Pages/home/Home'
-import Ourprojects from '../src/Pages/our-projects/ourprojects'
+import Ourprojects from '../src/Pages/our-projects/Ourprojects'
 import Services from '../src/Pages/services/Services'
 import Contact from '../src/Pages/contact/Contact'
 
